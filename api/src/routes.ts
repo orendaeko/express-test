c0a1a39d87ca43115fa351d134d1eb9de28edd80
Eko Nur Arifin <eko@orenda.co.id>
2022-10-18 17:51:31 +0700

    vercel

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.use('/', (req, res) => {
-  res.send('Hello there');
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.json({ name: 'Hello John Doe' });
 });
 
 export default router;
