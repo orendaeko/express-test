e7dc7869be654581d2b02085b56b76c236ab1f39
Eko Nur Arifin <70499417+orendaeko@users.noreply.github.com>
2022-10-19 11:41:01 +0700

    res

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const app = require('express')();
 
 app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.json(`Hello! `);
+  res.json({ res: `Hello! ` });
 });
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
-  res.json(`Item: ${slug}`);
+  res.json({ res: `Item: ${slug}` });
 });
 
 module.exports = app;
